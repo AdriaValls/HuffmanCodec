fd3c5ef43aad0b4aa8637ca06871377adf29ebe8
AdriaValls <avallsmo12@alumnes.ub.edu>
2022-03-31 20:34:06 +0200

    Methods to calculate the entropy of a table Symbol/Frequency and a method to calculate the number of bits needed to encode uncompressed Symbols from the table.

--- src/Huffman.java
+++ src/Huffman.java
@@ -8,23 +8,26 @@ public class Huffman {
         HashMap<String,Float> symbolFreqTable;
         HuffTree hTree;
 
-        //generating the Symbol/Frequency table
+        //generating the Symbol/Frequency table.
         HuffTables tables = new HuffTables();
         symbolFreqTable = tables.getFreqTable();
 
+        //Number of bits for symbol without compressing and Entropy.
+        int bitsBfCompression = numBitsForDecoding(symbolFreqTable);
+        float entropy = entropyFromFrequencyTable(symbolFreqTable);
+        System.out.print(entropy);
+
+        //Creating a tree and a Symbol/Code table
         hTree = new HuffTree(symbolFreqTable);
         codeTable = hTree.getCodeTable();
         System.out.print(codeTable +"\n");
 
-
-        //String sequence = "DQTD9";
+        //String sequence = "DQTD9"
         String sequence = "00110100000101";
-
         String bits= bitsToSymbols(hTree, sequence);
         System.out.print(bits);
 
     }
-
 
     public static String bitsToSymbols(HuffTree tree, String bits){
         return  tree.symbolsFromBits(bits);
@@ -39,6 +42,23 @@ public class Huffman {
         return bits;
     }
 
+    public static float entropyFromFrequencyTable(HashMap<String,Float> freqTable){
+        float entropy = 0;
+
+        for(String key : freqTable.keySet()){
+            float prob = freqTable.get(key);
+            double bitNeeded =  prob * (Math.log(prob) / Math.log(2));
+            entropy += bitNeeded;
+        }
+
+        return -1*entropy;
+    }
+
+    public static int numBitsForDecoding(HashMap<String,Float> freqTable){
+        int totalElements = freqTable.size();
+        double bitNeeded =  Math.round(Math.log(totalElements) / Math.log(2));
+        return (int) bitNeeded;
+    }
 
 
 }
